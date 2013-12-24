980d2f735564864a97b3fbc31b1d1c28675a49ef
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2013-12-24 16:44:47 +0200

    changed binary paths in build scripts

--- csrc/pmurhash/build-mingw32.sh
+++ csrc/pmurhash/build-mingw32.sh
@@ -1,3 +1,3 @@
-gcc PMurHash.c -O3 -s -shared -o ../../bin/pmurhash.dll
+gcc PMurHash.c -O3 -s -shared -o ../../bin/mingw32/pmurhash.dll
 
-cd ../.. && bin/luajit murmurhash3_test.lua
+#cd ../.. && bin/luajit murmurhash3_test.lua
