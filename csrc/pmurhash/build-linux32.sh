00b4d352b9b919a17b72413b7b208e004d9ba84d
Cosmin Apreuetsei <cosmin.apreutesei@gmail.com>
2014-01-14 14:41:22 +0200

    added $CFLAGS option and recompiled with -O2

--- csrc/pmurhash/build-linux32.sh
+++ csrc/pmurhash/build-linux32.sh
@@ -1,3 +1,3 @@
-gcc PMurHash.c -O3 -s -shared -o ../../bin/linux32/libpmurhash.so
+gcc $CFLAGS PMurHash.c -shared -o ../../bin/linux32/libpmurhash.so
 
 #cd ../.. && linux/bin/luajit murmurhash3_test.lua
--- csrc/pmurhash/build-mingw32.sh
+++ csrc/pmurhash/build-mingw32.sh
@@ -1,3 +1,3 @@
-gcc PMurHash.c -O3 -s -shared -o ../../bin/mingw32/pmurhash.dll
+gcc $CFLAGS PMurHash.c -shared -o ../../bin/mingw32/pmurhash.dll
 
 #cd ../.. && bin/luajit murmurhash3_test.lua
