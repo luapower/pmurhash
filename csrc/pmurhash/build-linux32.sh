f9e773d69e9179f6023f956be37a076790138776
Cosmin Apreuetsei <cosmin.apreutesei@gmail.com>
2014-01-23 00:52:32 +0200

    removed CFLAGS/CXXFLAGS

--- csrc/pmurhash/build-linux32.sh
+++ csrc/pmurhash/build-linux32.sh
@@ -1,3 +1,3 @@
-gcc $CFLAGS PMurHash.c -shared -o ../../bin/linux32/libpmurhash.so
+gcc -O2 -s -static-libgcc PMurHash.c -shared -o ../../bin/linux32/libpmurhash.so
 
 #cd ../.. && linux/bin/luajit murmurhash3_test.lua
--- csrc/pmurhash/build-mingw32.sh
+++ csrc/pmurhash/build-mingw32.sh
@@ -1,3 +1,3 @@
-gcc $CFLAGS PMurHash.c -shared -o ../../bin/mingw32/pmurhash.dll
+gcc -O2 -s -static-libgcc PMurHash.c -shared -o ../../bin/mingw32/pmurhash.dll
 
 #cd ../.. && bin/luajit murmurhash3_test.lua
