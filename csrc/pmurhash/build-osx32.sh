e8c5d30678e1bf7d179c2895cb071864042501f7
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2014-03-19 23:10:41 +0200

    osx32 build

---
+++ csrc/pmurhash/build-osx32.sh
@@ -0,0 +1,1 @@
+gcc -arch i386 -O2 PMurHash.c -shared -o ../../bin/osx32/libpmurhash.dylib
--- csrc/pmurhash/build-osx64.sh
+++ csrc/pmurhash/build-osx64.sh
@@ -1,1 +1,1 @@
-gcc -O2 PMurHash.c -shared -o ../../bin/osx64/libpmurhash.dylib
+gcc -arch x86_64 -O2 PMurHash.c -shared -o ../../bin/osx64/libpmurhash.dylib
