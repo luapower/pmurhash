216b22df70cc769e2c79a69095bc2299c8013d04
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2014-03-26 20:09:18 +0200

    rebuild for osx with @loader_path-based install_name

--- csrc/pmurhash/build-osx32.sh
+++ csrc/pmurhash/build-osx32.sh
@@ -1,1 +1,1 @@
-gcc -arch i386 -O2 PMurHash.c -shared -o ../../bin/osx32/libpmurhash.dylib
+gcc -arch i386 -O2 PMurHash.c -shared -install_name @loader_path/libpmurhash.dylib -o ../../bin/osx32/libpmurhash.dylib
--- csrc/pmurhash/build-osx64.sh
+++ csrc/pmurhash/build-osx64.sh
@@ -1,1 +1,1 @@
-gcc -arch x86_64 -O2 PMurHash.c -shared -o ../../bin/osx64/libpmurhash.dylib
+gcc -arch x86_64 -O2 PMurHash.c -shared -install_name @loader_path/libpmurhash.dylib -o ../../bin/osx64/libpmurhash.dylib
